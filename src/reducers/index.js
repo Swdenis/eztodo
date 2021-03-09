9abac2e5643b8ba87e0b1d6e0d5d7d52effe91db
Swdenis <swdenis@mail.ru>
2021-03-09 10:31:52 +0100

    added auth reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 import itemReducer from './itemReducer';
 
 export default combineReducers(
     {
-        items: itemReducer
+        items: itemReducer,
+        auth: authReducer
     }
 )
