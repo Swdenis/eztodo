f7205df5a11687ff52228e00bbbc34407ed5e460
Swdenis <swdenis@mail.ru>
2021-03-15 10:38:58 +0100

    Added login page styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,7 @@ import { Formik, Form } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { signIn, tryLogIn } from '../actions';
 import * as Yup from 'yup' 
-import { Button, Container, Header, Segment } from 'semantic-ui-react';
+import { Button, Container, Header, Segment, FormGroup } from 'semantic-ui-react';
 import history from '../history'
 import MyTextInput from './MyTextInput';
 
@@ -47,10 +47,13 @@ export default function Login() {
        }) => (
          <Form className='ui form' onSubmit={handleSubmit}>
             <Header as='h2' textAlign='center' content='Login to app' />
-            <MyTextInput name='email' placeholder='Email' />
-            <MyTextInput name= 'password' placeholder='Password' type='password' />
-           {errors.email && touched.email && errors.email}
-           {errors.password && touched.password && errors.password}
+            <Container style={{  display: "flex", justifyContent: "center", alignItems: "center", 
+            alignContent: "center", height: "100%", position: "relative", flexWrap: "wrap", flexDirection:"column"}}>
+                <MyTextInput name='email' placeholder='Email' />
+                <MyTextInput name= 'password' placeholder='Password' type='password' />
+              {errors.email && touched.email && errors.email}
+              {errors.password && touched.password && errors.password}
+            </Container>
            <Button positive type="submit" disabled={isSubmitting}>
              Log in
            </Button>
